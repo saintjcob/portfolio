c5c8fc7491ba215b07d0f79e40ea2c9a93788518
Jakub Ziemba <ziemba.jak@gmail.com>
2025-10-08 22:18:45 +0200

    fix: update about section

--- app/_components/about-section.tsx
+++ app/_components/about-section.tsx
@@ -1,15 +1,15 @@
-import LinkAnimated from "./link-animated";
+// import LinkAnimated from "./link-animated";
 
 export default function AboutSection() {
   return (
     <div className="about-section max-w-[600px] leading-relaxed tracking-text text-foreground [perspective:400px]">
       <h1 className="text-2xl font-medium">Jakub Ziemba</h1>
       <p className="pt-4">Design Engineer based in Warsaw</p>
-      <p className="text-sm text-foreground-dimmed">
+      {/* <p className="text-sm text-foreground-dimmed">
         <span className="text-foreground-dimmed">Currently:</span> Design
         Engineer @{" "}
         <LinkAnimated href="https://www.blazity.com">Blazity</LinkAnimated>
-      </p>
+      </p> */}
       {/* <p className="text-sm text-foreground-dimmed">
         <span className="text-foreground-dimmed">Previously:</span> Web
         Developer @{" "}
